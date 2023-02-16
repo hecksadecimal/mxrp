d40ff76e95de7cfe939fba6e722559d3a01da58e
Hex <hecksadecimal@outlook.com>
2023-02-15 22:59:40 -0500

    Guh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,1 @@
-if [[ ! -n "${MXRP_ENTRYPOINT}" ]]; then
-    echo "Running $MXRP_ENTRYPOINT"
-    eval "${MXRP_ENTRYPOINT}"
-else
-    echo "MXRP_ENTRYPOINT undefined!"
-fi 2>&1
+eval "${MXRP_ENTRYPOINT}"
